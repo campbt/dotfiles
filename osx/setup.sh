d277ae61ef7ce1a745f1ed07c07cf5daca5dc001
Tyler Campbell <tyler.campbell@theice.com>
2017-12-19 16:37:10 -0500

    Added setup.sh and updated README

---
+++ osx/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ln -s ~/dotfiles/osx/vim/.vim ~/.vim
+ln -s ~/dotfiles/osx/vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/osx/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/osx/zsh/.zsh ~/.zsh
+ln -s ~/dotfiles/osx/tmux/.tmux.config ~/.tmux.config
+mkdir ~/.vim/tmp
+mkdir ~/.vim/tmp/swap
+mkdir ~/.vim/tmp/undo
+mkdir ~/.vim/tmp/backup
+# ln ~/dotfiles/osx/config.fish ~/.config/fish/config.fish
